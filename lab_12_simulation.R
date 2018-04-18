c7b70f5068ad9ea372b859eb415b17d44903150c
Kady Yip <kadyy1998@gmail.com>
2018-04-18 00:43:25 -0400

    Created model_select function

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -1,5 +1,17 @@
 generate_data = function(n, p) {
   covariates = matrix(rnorm(n*p), nrow=n, ncol=p)
   responses = rnorm(n)
-  return(list(covariates, responses))
+  return(list(covariates=covariates, responses=responses))
+}
+
+model_select = function(covariates, responses, cutoff) {
+  all = lm(responses ~ covariates)
+  coefs = coef(all)[-1] # remove intercept
+  cov.reduced.ind = which(coefs <= cutoff)
+  if (length(cov.reduced.ind) == 0) return(c())
+  else {
+    reduced.model = lm(responses ~ covariates[,cov.reduced.ind])
+    coefs.reduced.model = coef(reduced.model)
+    return(coefs.reduced.model)
+  }
 }
